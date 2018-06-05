d70691c189be28d31fdd5e1b89735f3cbde3af38
graceyng <graceyng14@gmail.com>
2018-06-05 22:39:52 +0000

    pushing the wrapper file cause it was never pushed

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,8 @@
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 import nibabel as nib
 import numpy as np
+import cv2
 from PIL import Image
 
 class OASIS(Dataset):
@@ -19,19 +21,26 @@ class OASIS(Dataset):
         self.labels = []
         self.mean_pixel_threshold = mean_pixel_threshold
         self.discard_front_proportion = discard_front_proportion
+        #resize to 360 and then crop to 299, which is the input size for inception
+        self.transform = transforms.Compose([transforms.Resize(299),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize([0.5,0.5,0.5],[0.25,0.25,0.25])])
+                                            
 
         for filename, label in filename_label_list:
-            select_slices(filename, label)
+            OASIS.select_slices(self, filename, label)
 
     def select_slices(self, filename, label):
-        # TODO
         img = OASIS.get_image(filename)
         img = OASIS.crop_image(img)
-        img = OASIS.resize_and_scale_image(img)
-        slice_indices = OASIS.filter_slices(img)
+
+        slice_indices = OASIS.filter_slices(self, img)
+        num_slices = min(15, len(slice_indices))
+        slice_indices = np.random.choice(slice_indices, num_slices)
+
         for idx in slice_indices:
-            # self.slices append img[idx]
-            # self.labels append label
+            self.slices.append(img[:,:,idx])
+            self.labels.append(label)
 
     @staticmethod
     def get_image(path):
@@ -41,22 +50,35 @@ class OASIS(Dataset):
 
     @staticmethod
     def crop_image(img):
-        # TODO
-        return img
+        width, height, depth  = img.shape
+        # return unchanged if square
+        if width == height:
+            return img
+        else:
+            #calculate the margin and then the two offsets
+            margin = abs(width - height)
+            front_offset = margin // 2
+            back_offset = margin - front_offset
+            if width > height:
+                return img[front_offset:-back_offset,:,:]
+            else:
+                return img[:,front_offset:-back_offset,:]
 
     @staticmethod
-    def filter_slices(img):
+    def filter_slices(self, img):
         img_means = img.reshape(-1, img.shape[2]).mean(axis=0)
         # zero out first discard_front_proportion
         front_index = int(self.discard_front_proportion * img_means.shape[0])
         img_means[:front_index] = 0
         threshold = img_means.max() * self.mean_pixel_threshold
-        return img_means > threshold
+        return np.arange(img.shape[2])[(img_means > threshold)]
 
     @staticmethod
     def resize_and_scale_image(img):
-        # TODO: figure out what to resize to
-        # TODO: figure out what to rescale to
+        # resize to (299,299)
+        rsz = cv2.resize(img, dsize = (299,299))
+        # rescale to 360
+        transforms.Resize(360)
         return img
 
     def __getitem__(self, index):
@@ -65,7 +87,10 @@ class OASIS(Dataset):
         # If dataset is preloaded
         img_slice = self.slices[index]
         # convert img_slice to 3-channel
-        #     image = np.dstack([img[:,:,i]] * 3)
+        img_slice = np.dstack([img_slice] * 3)
+
+        img_slice = Image.fromarray(img_slice)
+        img_slice = self.transform(img_slice)
 
         label = self.labels[index]
         return img_slice, label
